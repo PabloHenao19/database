44dbcd1314835115269d104b7e663c44ff6d2a7e
PabloHenao19 <henaopablo16@gmail.com>
2023-06-07 00:45:21 -0500

    Conexión database

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link rel="stylesheet" href="style.css">    
+</head>
+<body>
+
+<h1>LOGIN</h1>
+<span>or <a href="signup.php">SignUp</a></span>
+    <form action="login.php" method="post">
+<input type="text" name="email" placeholder="Ingresar correo">
+<input type="password" name="password" placeholder="Ingresar contraseña">
+<input type="submit" value="Send">
+
+    </form>
+    
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,49 @@
+<?php
+
+  require 'database.php';
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+
+    if ($stmt->execute()) {
+      $message = 'USUARIO CREADO CON EXTIO';
+    } else {
+      $message = 'ERROR DE CONEXIÓN';
+    }
+  }
+
+
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php if(!empty($message)): ?>
+  <p> <?= $message ?></p>
+<?php endif; ?>
+
+<h1>SignUp</h1>
+    <span>or <a href="login.php">Login</a></span>
+
+    <form action="signup.php" method="post">
+<input type="text" name="email" placeholder="Ingresar correo">
+<input type="password" name="password" placeholder="Ingresar contraseña">
+<input type="password" name="confirm_password" placeholder="Confirmar tu contraseña">
+<input type="submit" value="Send">
+
+    </form>
+
+
+</body>
+</html>
